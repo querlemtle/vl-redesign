8208eec1c27425862762a31a65a6bf143a5d2cf6
querlemtle <72937427+querlemtle@users.noreply.github.com>
2024-05-01 14:02:33 +0800

    fix: modulize news page layout

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,35 +1,48 @@
 import newsTitle from "./../assets/news-page-title.svg";
+import tempNewsBanner from "./../assets/temp-news-banner.png";
+import tempDiscordBanner from "./../assets/temp-discord-banner.png";
 import NewsCard from "../components/NewsCard";
-import "./News.css";
+import styles from "./News.module.css";
+
+const {
+  banner,
+  section__title: sectionTitle,
+  "section__title--subtitle-right": sectionTitleSubtitleRight,
+  "section__title--stretch": sectionTitleStretch,
+  "section__bg--primary-color-light-10": sectionBgPrimaryColorLight10,
+  pagination,
+  "pagination__control": paginationControl,
+  aside
+} = styles;
 
 export default function News() {
   return (
     <>
-      <section className="section">
-        Img here.
+      <section className={banner}>
+        <img src={tempNewsBanner} alt="News page banner" />
       </section>
-      <section className="section__cols-2 section__bg--primary-color-light-10">
-        <h1 className="section__title section__title--stretch section__title--subtitle-right">
-          <img src={newsTitle} alt="News" className="section___title-img" />
+      <section className={`section__cols-2 ${sectionBgPrimaryColorLight10}`}>
+        <h1 className={`${sectionTitle} ${sectionTitleSubtitleRight} ${sectionTitleStretch}`}>
+          <img src={newsTitle} alt="News" />
         </h1>
         <NewsCard />
         <NewsCard />
         <NewsCard />
         <NewsCard />
-        <ul className="pagination">
-          <a href="#" className="pagination__control">&#8592;</a>
+        <ul className={pagination}>
+          <a href="#" className={paginationControl}>&#8592;</a>
           <li><a href="#">1</a></li>
           <li><a href="#">2</a></li>
           <li><a href="#">3</a></li>
           <li><a href="#">...</a></li>
           <li><a href="#">9</a></li>
           <li><a href="#">10</a></li>
-          <a href="#" className="pagination__control">&#8594;</a>
+          <a href="#" className={paginationControl}>&#8594;</a>
         </ul>
       </section>
-      <aside className="aside">
-        <a href="#" className="aside__link">
-          Discord banner here.
+      <aside className={aside}>
+        <a href="https://discord.gg/ECAdMaTNjT" className="aside__link">
+          <img src={tempDiscordBanner} alt="加入 Vlive Lab 官方 Discord" />
         </a>
       </aside>
     </>
