f94eeef3621fdbdde754a31683196417de87e12f
querlemtle <72937427+querlemtle@users.noreply.github.com>
2024-05-26 16:05:32 +0800

    refactor: talents data structure and long date formatter

--- src/pages/Talent.jsx
+++ src/pages/Talent.jsx
@@ -1,17 +1,10 @@
 import PropTypes from "prop-types";
 import styles from "./Talent.module.css";
-import {
-  talentVid,
-  sandersBg,
-  hanakawaBg,
-  sandersPortrait,
-  hanakawaPortrait,
-  sandersLogo,
-  hanakawaLogo,
-  flowersDeco,
-} from "../assets/images";
+import { talentVid, flowersDeco } from "../assets/images";
+import talentsData from "./../data/talentsData";
+import { formatDateLong } from "./../utils/formatDate";
 import { FbIcon, XIcon, YtIcon } from "../components/IconSvgs";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
@@ -68,7 +61,7 @@ function CharBio({
   charIntro,
   textColor,
   isPortraitAtRight,
-  timestamp,
+  debutDate,
   ytLink,
   fbLink,
   xLink,
@@ -87,7 +80,6 @@ function CharBio({
     });
   });
 
-
   return (
     <section
       style={{ backgroundImage: `url(${bgUrl})` }}
@@ -96,11 +88,7 @@ function CharBio({
     >
       {/* 角色圖 */}
       <div className={`${cardImg} ${isPortraitAtRight && cardImgRight}`}>
-        <img
-          src={flowersDeco}
-          alt="flowers"
-          className={cardImgBg}
-        />
+        <img src={flowersDeco} alt="flowers" className={cardImgBg} />
         <img
           src={portraitUrl}
           alt="portrait"
@@ -114,7 +102,7 @@ function CharBio({
         <img src={logoUrl} alt="logo" />
         <p className={cardContext}>{charIntro}</p>
         <p>
-          &#9632; 初配信時間： <span>{timestamp}</span>
+          &#9632; 初配信時間： <span>{debutDate}</span>
         </p>
         <div>
           <IconLinks
@@ -137,7 +125,7 @@ CharBio.propTypes = {
   charIntro: PropTypes.string.isRequired,
   textColor: PropTypes.string.isRequired,
   isPortraitAtRight: PropTypes.bool.isRequired,
-  timestamp: PropTypes.string.isRequired,
+  debutDate: PropTypes.string.isRequired,
   ytLink: PropTypes.string.isRequired,
   fbLink: PropTypes.string.isRequired,
   xLink: PropTypes.string.isRequired,
@@ -145,6 +133,12 @@ CharBio.propTypes = {
 
 export default function Talent() {
   const container = useRef();
+  const [talents, setTalents] = useState(
+    // 只顯示前兩位藝人資料（因為第三位資料不全）
+    talentsData.filter((talent, i) => {
+      return i <= 1;
+    })
+  );
 
   useGSAP(() => {
     gsap.to("[data-ani='clip']", {
@@ -171,36 +165,28 @@ export default function Talent() {
       <section className={bgBlack}>
         <video muted autoPlay loop src={talentVid} className={mask} />
         <h3 className={textStacking}>
-          以SCP基金會的世界觀為基礎，在該虛構宇宙中，SCP基金會是
-          一個跨國秘密組織，負責搜尋並收容各種具有異常屬性的個體、
-          地點或物體。VTuber們將會探索和解析不尋常的事件和對象。
+          以SCP基金會的世界觀為基礎，在該虛構宇宙中，SCP基金會是一個跨國秘密組織，負責搜尋並收容各種具有異常屬性的個體、地點或物體。VTuber們將會探索和解析不尋常的事件和對象。
           在直播中進行各種研究、調查活動，並分享新的發現和冒險。
         </h3>
       </section>
-      <CharBio
-        bgUrl={sandersBg}
-        portraitUrl={sandersPortrait}
-        logoUrl={sandersLogo}
-        charIntro="桑德斯是一位遊戲開發者，長期在遊戲業不得志。某次他熱心地替別的專案幫忙，卻反遭設局陷害，這對他造成巨大的打擊，他的陰暗人格「桑德斯·闇」就此誕生。這個人格是一個想要毀滅世界的魔王，他曾經因此想過要引爆地心，甚至是毀滅太陽，看人類在絕望中滅亡…"
-        textColor="#fff"
-        isPortraitAtRight={false}
-        timestamp="2023/7/16 7:00PM"
-        ytLink="https://www.youtube.com/@GameSeasoning"
-        fbLink="https://www.facebook.com/GameSeasoning/"
-        xLink="https://twitter.com/GameSeasoning66"
-      />
-      <CharBio
-        bgUrl={hanakawaBg}
-        portraitUrl={hanakawaPortrait}
-        logoUrl={hanakawaLogo}
-        charIntro="從忘川-彼岸花剛解除封印的冥界魔龍，因為剛解除封印所以沒什麼力量，除了外表長得像魔龍，其他能力都和普通少女差不多。有使用冥界夢境的能力(產生夢境迷霧直播連線)，每天都在夢境直播中跟在世的人託夢，也常常被託夢說刮刮樂或抽獎會中獎，常被認識的說抽卡歐皇。"
-        textColor="#112e44"
-        isPortraitAtRight={true}
-        timestamp="2023/10/10 7:00PM"
-        ytLink="https://www.youtube.com/@hanakawa0516"
-        fbLink="https://www.facebook.com/profile.php?id=100094968534135"
-        xLink="https://twitter.com/hanakawa0516"
-      />
+      {talents.map((talent, i) => {
+        return (
+          <CharBio
+            key={talent.id}
+            bgUrl={talent.images.bg}
+            portraitUrl={talent.images.portrait}
+            logoUrl={talent.images.logo}
+            textColor={talent.color}
+            // index 為偶數 => portrait @left，奇數 => portrait @right
+            isPortraitAtRight={i % 2 === 0 ? false : true}
+            charIntro={talent.description}
+            debutDate={formatDateLong(talent.debutDate)}
+            ytLink={talent.bioLinks.yt}
+            fbLink={talent.bioLinks.fb}
+            xLink={talent.bioLinks.x}
+          />
+        );
+      })}
     </>
   );
 }
